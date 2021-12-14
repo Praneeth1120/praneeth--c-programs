4b51def000e2e63b63317606b7dfb08344a2143a
Praneeth1120 <94948519+Praneeth1120@users.noreply.github.com>
2021-12-14 16:46:50 +0530

    Update 54) vowel or not using switch case.cpp

--- 54) vowel or not using switch case.cpp
+++ 54) vowel or not using switch case.cpp
@@ -19,3 +19,20 @@ main()
 		default : printf("Consonant");	
 	}
 }
+
+	/*switch(ch)
+	{
+		case 'a':
+		case 'e':
+		case 'i':
+		case 'o':
+		case 'u':
+		case 'A':
+		case 'E':
+		case 'I':
+		case 'O':
+		case 'U':printf("Vowel");
+				break;
+		default : printf("Consonant");
+				break;
+	}*/
